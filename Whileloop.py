667b8aa9046164b9c34ff31add9785b77a04600b
Kingeden1 <stewjf@yahoo.com>
2025-03-06 20:21:15 +0300

    py9

---
+++ Whileloop.py
@@ -0,0 +1,11 @@
+print("while loop")
+i=1 #initialization variable
+while i<=10: #condition
+    print("Iteration",i)
+    i+=1 #Increament
+
+print("while loop")
+i=10 #initialization variable
+while i>=1: #condition
+    print("Iteration",i)
+    i-=1 #Decreament
