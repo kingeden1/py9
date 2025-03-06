eb1087b243fe978f532ee6a111e5e09a0a4ab772
Kingeden1 <stewjf@yahoo.com>
2025-03-06 20:40:54 +0300

    py9

---
+++ Sum natural numbers.py
@@ -0,0 +1,8 @@
+print("sum of natural numbers:")
+num=int(input("enter number till which you must sum:"))
+sum=0
+i=1
+while i<=num:
+ sum=sum+i
+ i+=1
+ print("sum of first",num,"natural numbers:",sum)
